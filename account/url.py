0267c7c619f8ac30a9d74f9a22f63c7dfdd84f95
smilejakdu <ash982416@gmail.com>
2020-04-26 23:35:36 +0900

    sign_in

--- account/url.py
+++ account/url.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import *
+from .views      import (SignUpView,
+                         SignInView,
+                         KakaoView,
+                         ProfileView)
 
 urlpatterns = [
+    path('sign_up',     SignUpView.as_view()),
+    path('sign_in',     SignInView.as_view()),
+    path('kakao_login', KakaoView.as_view()),
+    path('my_info',     ProfileView.as_view())
 ]
