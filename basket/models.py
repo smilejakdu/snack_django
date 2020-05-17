b893214c3308694d42e42c8948d586ddbdb01579
smilejakdu <ash982416@gmail.com>
2020-05-17 21:40:25 +0900

    git_test

--- basket/models.py
+++ basket/models.py
@@ -4,8 +4,8 @@ from product.models import Product
 
 # Create your models here.
 class Basket(models.Model):
-    user       = models.ForeignKey('account.Account',     on_delete=models.SET_NULL,  null=True)
-    product    = models.ForeignKey('product.Product',     on_delete = models.CASCADE, null = True)
+    user       = models.ForeignKey('account.Account', on_delete=models.SET_NULL,  null=True)
+    product    = models.ForeignKey('product.Product', on_delete = models.CASCADE, null = True)
     quantity   = models.CharField(max_length=250, null=True)
     created_at = models.DateTimeField(auto_now_add = True)
 
